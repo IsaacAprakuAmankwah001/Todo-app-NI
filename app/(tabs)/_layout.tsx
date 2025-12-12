fae42ece88d5e5d3813bd7cf1e771d86f5205ed3
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2025-12-12 10:13:46 +0000

    Designs Tabs and  Homescreen

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,34 @@
+//import { View, Text } from 'react-native'       
+import { Ionicons } from '@expo/vector-icons'
+import { Tabs } from 'expo-router'
+import React from 'react'
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+        screenOptions={{}}
+    >
+        <Tabs.Screen 
+            name="index" 
+            options={{
+                title: 'Todos',
+                tabBarIcon: ({color, size}) =>(
+                    <Ionicons name="flash-outline" size={size} color={color} />
+                ) ,
+            }} 
+        />
+
+         <Tabs.Screen 
+            name="settings" 
+            options={{
+                title: 'Settings',
+                tabBarIcon: ({color, size}) =>(
+                    <Ionicons name="settings" size={size} color={color}/>
+                ) ,
+            }} 
+        />
+    </Tabs>
+  )
+}
+
+export default TabsLayout
