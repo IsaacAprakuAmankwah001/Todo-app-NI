cc4f886ac9ad995ee6e98fac396ec083e646c02d
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2025-12-31 20:13:16 +0000

    Debug

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { createSettingsStyles } from '@/assets/styles/settings.styles';
+import useTheme from '@/hooks/useTheme';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
 
 const SettingScreen = () => {
+  const [isAutoSync, setIsAutoSync] = useState(true);
+  const [isNotificationsEnabled, setIsNotificationsEnabled] = useState(true);
+
+  const {colors,isDarkMode,toggleDarkMode } = useTheme();
+
+  const settingsStyles = createSettingsStyles(colors);
   return (
     <View>
       <Text>SettingScreen</Text>
