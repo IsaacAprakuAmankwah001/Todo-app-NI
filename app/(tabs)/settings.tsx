e10aae75f3bff0c313625fbe2374c97be17346ff
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-01 11:54:35 +0000

    Debug

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -5,34 +5,36 @@ import useTheme from '@/hooks/useTheme';
 import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import { ScrollView, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const SettingScreen = () => {
   const {colors } = useTheme();
 
   const settingsStyles = createSettingsStyles(colors);
   return (
-    <LinearGradient colors={colors.gradients.background} style={settingsStyles.container}>
-      <SafeAreaView style={settingsStyles.safeArea}>
-        <View style={settingsStyles.header}>
-          <View style={settingsStyles.titleContainer}>
-            <LinearGradient colors={colors.gradients.primary} style={settingsStyles.iconContainer}>
-              <Ionicons name="settings-sharp" size={28} color="#ffffff" />
-            </LinearGradient>
-            <Text style={settingsStyles.title}>Settings</Text>
+    <SafeAreaProvider>
+      <LinearGradient colors={colors.gradients.background} style={settingsStyles.container}>
+        <SafeAreaView style={settingsStyles.safeArea}>
+          <View style={settingsStyles.header}>
+            <View style={settingsStyles.titleContainer}>
+              <LinearGradient colors={colors.gradients.primary} style={settingsStyles.iconContainer}>
+                <Ionicons name="settings-sharp" size={28} color="#ffffff" />
+              </LinearGradient>
+              <Text style={settingsStyles.title}>Settings</Text>
+            </View>
           </View>
-        </View>
 
-        <ScrollView 
-        style={settingsStyles.scrollView} 
-        contentContainerStyle={settingsStyles.content}
-        showsVerticalScrollIndicator={false}
-        >
-          <ProgressStats />
-          <Preferences />
-        </ScrollView>
-      </SafeAreaView>
-    </LinearGradient>
+          <ScrollView 
+          style={settingsStyles.scrollView} 
+          contentContainerStyle={settingsStyles.content}
+          showsVerticalScrollIndicator={false}
+          >
+            <ProgressStats />
+            <Preferences />
+          </ScrollView>
+        </SafeAreaView>
+      </LinearGradient>
+    </SafeAreaProvider>
   )
 }
 
