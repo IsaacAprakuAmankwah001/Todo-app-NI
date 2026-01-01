55dc06a240bdc5c1273464381fcb9c7106afe5de
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-01 10:28:15 +0000

    Added Progress Stats

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,7 +1,10 @@
 import { createSettingsStyles } from '@/assets/styles/settings.styles';
+import ProgressStats from '@/components/ProgressStats';
 import useTheme from '@/hooks/useTheme';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 
 const SettingScreen = () => {
   const [isAutoSync, setIsAutoSync] = useState(true);
@@ -11,9 +14,27 @@ const SettingScreen = () => {
 
   const settingsStyles = createSettingsStyles(colors);
   return (
-    <View>
-      <Text>SettingScreen</Text>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={settingsStyles.container}>
+      <SafeAreaView style={settingsStyles.safeArea}>
+        <View style={settingsStyles.header}>
+          <View style={settingsStyles.titleContainer}>
+            <LinearGradient colors={colors.gradients.primary} style={settingsStyles.iconContainer}>
+              <Ionicons name="settings-sharp" size={28} color="#ffffff" />
+            </LinearGradient>
+            <Text style={settingsStyles.title}>Settings</Text>
+          </View>
+        </View>
+
+        <ScrollView 
+        style={settingsStyles.scrollView} 
+        contentContainerStyle={settingsStyles.content}
+        showsVerticalScrollIndicator={false}
+        >
+          <ProgressStats />
+
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   )
 }
 
---
+++ components/ProgressStats.tsx
@@ -0,0 +1,28 @@
+import { createSettingsStyles } from '@/assets/styles/settings.styles';
+import { api } from '@/convex/_generated/api';
+import useTheme from '@/hooks/useTheme';
+import { useQuery } from 'convex/react';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { Text } from 'react-native';
+
+const ProgressStats = () => {
+    const {colors} = useTheme();
+    const settingsStyles = createSettingsStyles(colors);
+
+    const todos = useQuery(api.todos.getTodos);
+    const totalTodos = todos ? todos.length : 0;
+    const completedTodos = todos ? todos.filter(todo => todo.isCompleted).length : 0;
+    const activeTodos = totalTodos - completedTodos;
+
+  return (
+    <LinearGradient colors={colors.gradients.surface} style={settingsStyles.section}>
+        <Text style={settingsStyles.sectionTitle}>Progress Stats</Text>
+        <LinearGradient colors={colors.gradients.background} style={[settingsStyles.statCard, {borderLeftColor: colors.primary}]}>
+
+        </LinearGradient>
+
+    </LinearGradient>
+  )
+}
+export default ProgressStats
