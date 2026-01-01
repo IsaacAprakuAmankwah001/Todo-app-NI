2e34c47bca19c80f0f0b62210ce46f115462d92e
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-01 13:52:50 +0000

    Debug

--- components/DangerZone.tsx
+++ components/DangerZone.tsx
@@ -1,10 +1,11 @@
 import { createSettingsStyles } from '@/assets/styles/settings.styles';
 import { api } from '@/convex/_generated/api';
 import useTheme from '@/hooks/useTheme';
+import { Ionicons } from '@expo/vector-icons';
 import { useMutation } from 'convex/react';
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
-import { Alert, Text } from 'react-native';
+import { Alert, Text, TouchableOpacity, View } from 'react-native';
 
 const DangerZone = () => {
     const {colors} = useTheme();
@@ -40,7 +41,20 @@ const DangerZone = () => {
 
   return (
     <LinearGradient colors={colors.gradients.surface} style={settingsStyles.section}>
-        <Text style={settingsStyles.sectionTitle}>Danger Zone</Text>
+        <Text style={settingsStyles.sectionTitleDanger}>Danger Zone</Text>
+
+        <TouchableOpacity style={[settingsStyles.actionButton, {borderBottomWidth: 0}]} 
+        onPress={handleResetApp}
+        activeOpacity={0.7}
+        >
+            <View style={settingsStyles.actionLeft}>
+                <LinearGradient colors={colors.gradients.danger} style={settingsStyles.actionIcon}>
+                    <Ionicons name="trash" size={24} color="#ffffff" />
+                </LinearGradient>
+                <Text style={settingsStyles.actionText}>Reset App</Text>
+            </View>
+            <Ionicons name="chevron-forward" size={20} color={colors.textMuted} />
+        </TouchableOpacity>
     </LinearGradient>
   )
 }
